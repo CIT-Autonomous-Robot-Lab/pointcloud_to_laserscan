128ec8a3ec8c54269b140d015836ab7e4a426217
makotoyoshigoe <myoshigo0127@gmail.com>
2024-11-15 17:32:55 +0900

    Change param

--- launch/mid360_pointcloud2_to_laserscan.launch.py
+++ launch/mid360_pointcloud2_to_laserscan.launch.py
@@ -3,20 +3,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    
-    define_livox_frame = Node(
-        package = 'tf2_ros',
-        executable = 'static_transform_publisher',
-        name = 'define_livox_frame',
-        namespace = '',
-        output = 'screen',
-        arguments = [
-            '--x', '0.1012', '--y', '0', '--z', '0.177',
-            '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-            '--frame-id', 'base_link', '--child-frame-id', 'livox_frame',
-        ],
-    )
-
     pc2_to_scan = Node(
         package='pointcloud_to_laserscan',
         executable='pointcloud_to_laserscan_node',
@@ -24,16 +10,16 @@ def generate_launch_description():
         remappings=[('cloud_in', '/livox/lidar'),
                     ('scan', '/scan')],
         parameters=[{
-            'target_frame': 'livox_frame',
+            'target_frame': 'lidar_link',
             'transform_tolerance': 0.01,
-            'min_height': 0.0,
-            'max_height': 0.2,
+            'min_height': 0.3,
+            'max_height': 0.8,
             'angle_min': -3.1415,
             'angle_max': 3.1415,
             'angle_increment': 0.0087,  # M_PI/360.0
             'scan_time': 0.3333,
-            'range_min': 0.45,
-            'range_max': 10.0,
+            'range_min': 0.3,
+            'range_max': 100.0,
             'use_inf': True,
             'inf_epsilon': 1.0
         }],
@@ -41,7 +27,6 @@ def generate_launch_description():
     )
 
     ld = LaunchDescription()
-    ld.add_action(define_livox_frame)
     ld.add_action(pc2_to_scan)
 
     return ld
